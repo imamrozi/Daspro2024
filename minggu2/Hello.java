b5388bb887f6937bf58f2b40a44e430bde9605d9
imamrozi <imam.rozi@gmail.com>
2024-09-05 15:47:24 +0700

    change greeting

--- minggu2/Hello.java
+++ minggu2/Hello.java
@@ -3,6 +3,6 @@ package minggu2;
 class Hello{
     public static void main(String[] args) {
         System.out.println("Hello...");
-        System.out.println("Selamat sore!");
+        System.out.println("Selamat sore, apa kabar?");
     }
 }
